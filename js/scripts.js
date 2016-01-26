35201ff98a4232f84f49d41c98e3f128f0d9a733
Christine Tacha and Ryan King <tacha.christine@gmail.com>
2016-01-26 10:19:24 -0800

    add passing specs for showtimes

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,10 @@ function movieTicket(movieTitle, age, releaseDate, showtime) {
   this.showtime = showtime;
 }
 
+var testTicket = new movieTicket("movie1", "11", "newRelease", "matinee");
+var testTicket2 = new movieTicket("movie1", "55", "newRelease", "regular");
+var testTicket3 = new movieTicket("movie1", "25", "newRelease", "matinee");
+
 movieTicket.prototype.ticketType = function () {
   if (testTicket.age <= 12) {
     return "childPrice";
@@ -14,6 +18,38 @@ movieTicket.prototype.ticketType = function () {
     return "regularPrice";
   }
 }
+movieTicket.prototype.ticketType2 = function () {
+  if (testTicket2.age <= 12) {
+    return "childPrice";
+  } else if (testTicket2.age >= 55) {
+    return "seniorPrice";
+  } else {
+    return "regularPrice";
+  }
+}
+ movieTicket.prototype.ticketType3 = function () {
+   if (testTicket3.age <= 12) {
+     return "childPrice";
+  } else if (testTicket3.age >= 55) {
+     return "seniorPrice";
+  } else {
+     return "regularPrice";
+  }
+ }
+
+ movieTicket.prototype.showing = function () {
+  if (testTicket.showtime === "matinee") {
+    return "matinee";
+  } else
+    return "regular";
+  }
+
+  movieTicket.prototype.showing2 = function () {
+   if (testTicket2.showtime === "matinee") {
+     return "matinee";
+   } else
+     return "regular";
+   }
  // $(document).ready(function() {
  //   if (testTicket.age <= 12) {
  //   return "childPrice";
--- spec/specs.js
+++ spec/specs.js
@@ -6,9 +6,18 @@ describe("movieTicket",function(){
     expect(testTicket.releaseDate).to.equal("newRelease");
     expect(testTicket.showtime).to.equal("matinee");
   });
-  it("will determine if the customer is under 12 or over 55", function(){
-    var testTicket = new movieTicket("movie1", "11", "newRelease", "matinee");
+  it("will determine if the customer is 12 or younger", function(){
     expect(testTicket.ticketType()).to.equal("childPrice");
+  });
+  it("will determine if the customer is 55 or older", function(){
+    expect(testTicket2.ticketType2()).to.equal("seniorPrice");
+  });
+   it("will determine if the customer is between 12 and 55", function(){
+     expect(testTicket3.ticketType3()).to.equal("regularPrice");
+  });
+  it("will determine time of showing", function(){
+    expect(testTicket.showing()).to.equal("matinee");
+    expect(testTicket2.showing2()).to.equal("regular");
   });
 });
 
