f95bc6343da8720d1f16bbc697fbfab7fb2b8063
Christine Tacha and Ryan King <ryanking157@hotmail.com>
2016-01-26 16:07:54 -0800

    add object structure instead of jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,36 @@
-// function movieTicket(movieTitle, age, releaseDate, showtime) {
-//   this.movieTitle = movieTitle;
-//   this.age = age;
-//   this.releaseDate = releaseDate;
-//   this.showtime = showtime;
-// }
+function Ticket(age, movieTime, secondRelease) {
+  this.age = age;
+  this.movieTime = movieTime;
+  this.secondRelease = secondRelease;
+}
+
+Ticket.prototype.cost = function() {
+      if (this.secondRelease === ("secondRelease1" || "secondRelease2")) {
+        return "$6";
+      } else if ((this.age >= 55) && (this.movieTime === "matinee")) {
+        return "$8.50";
+      } else if ((this.age >= 55) && (this.movieTime === "evening")) {
+        return "$10.50";
+      } else if (this.age < 12) {
+        return "$6.50";
+      } else if ((this.age > 12) && (this.age < 55) && (this.movieTime === "evening")) {
+        return "$13.50";
+      } else {
+        return "$10.50";
+      }
+}
 
 $(function() {
   $("#ticketForm").submit(function(event) {
+
     var age = $('input[name=age]:checked', '#ticketForm').val();
     var movieTime = $('input[name=movieTime]:checked', '#ticketForm').val();
-    var newRelease = $('input[name=release]:checked', '#ticketForm').val();
     var secondRelease = $('input[name=release]:checked', '#ticketForm').val();
 
+    var userTicket = new Ticket(age, movieTime, secondRelease)
 
-    if (secondRelease === ("secondRelease1" || "secondRelease2")) {
-      $("#appendHere").text("$6");
-    } else if ((age >= 55) && (movieTime === "matinee")) {
-      $("#appendHere").text("$8.50");
-    } else if ((age >= 55) && (movieTime === "evening")) {
-      $("#appendHere").text("$10.50");
-    } else if (age < 12) {
-      $("#appendHere").text("$6.50");
-    } else if ((age > 12) && (age < 55) && (movieTime === "evening")) {
-      $("#appendHere").text("$13.50");
-    } else {
-      $("#appendHere").text("$10.50");
-    }
-
+    $("#appendHere").text(userTicket.cost());
+    
     event.preventDefault();
    });
  });
